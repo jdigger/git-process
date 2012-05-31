574161cd1c1797111b657fb5ab291509086b5b5e
Jim Moore <moore.jim@gmail.com>
2012-05-31 14:28:47 -0600

    Adds some logging

--- lib/git-process.rb
+++ lib/git-process.rb
@@ -1,20 +1,31 @@
 require "rubygems"
 require "bundler/setup"
+
 require 'rugged'
+require 'logger'
 
 module Git
 
   class Process
-    attr_reader :repo
+    attr_reader :repo, :logger
 
     def initialize(repo)
       @repo = repo
+
+      @logger = Logger.new(STDOUT)
+      @logger.datetime_format = "%Y-%m-%d %H:%M:%S"
+      f = Logger::Formatter.new
+      @logger.formatter = proc do |severity, datetime, progname, msg|
+        "#{severity[0..0]}: Git::Process #{datetime.strftime(@logger.datetime_format)}: #{msg}\n"
+      end
     end
         
 
     def self.create(dir)
       repo = Rugged::Repository.init_at(dir, false)
-      Git::Process.new(repo)
+      gp = Git::Process.new(repo)
+      gp.logger.debug {"git init #{dir}"}
+      gp
     end
 
 
