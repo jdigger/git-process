ea72d16615457c2b6cda1eb54af795b50ce8381c
Jim Moore <moore.jim@gmail.com>
2012-05-31 14:40:00 -0600

    Improves logging

--- lib/git-process.rb
+++ lib/git-process.rb
@@ -7,25 +7,26 @@ require 'logger'
 module Git
 
   class Process
+    @@logger = Logger.new(STDOUT)
+    @@logger.datetime_format = "%Y-%m-%d %H:%M:%S"
+    f = Logger::Formatter.new
+    @@logger.formatter = proc do |severity, datetime, progname, msg|
+      "#{severity[0..0]}: Git::Process #{datetime.strftime(@@logger.datetime_format)}: #{msg}\n"
+    end
+
     attr_reader :repo, :logger
 
     def initialize(repo)
       @repo = repo
-
-      @logger = Logger.new(STDOUT)
-      @logger.datetime_format = "%Y-%m-%d %H:%M:%S"
-      f = Logger::Formatter.new
-      @logger.formatter = proc do |severity, datetime, progname, msg|
-        "#{severity[0..0]}: Git::Process #{datetime.strftime(@logger.datetime_format)}: #{msg}\n"
-      end
+      @logger = @@logger
+      @logger.debug {"working dir: #{repo.workdir}"}
     end
         
 
     def self.create(dir)
+      @@logger.debug "git init"
       repo = Rugged::Repository.init_at(dir, false)
-      gp = Git::Process.new(repo)
-      gp.logger.debug {"git init #{dir}"}
-      gp
+      Git::Process.new(repo)
     end
 
 
