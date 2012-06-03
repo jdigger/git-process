c1109264de506c14438fb95a7289e0fc73935841
Jim Moore <moore.jim@gmail.com>
2012-06-02 21:02:12 -0600

    Splits part of git-process into lib.

--- lib/git-process.rb
+++ lib/git-process.rb
@@ -1,74 +1,40 @@
-require "rubygems"
-require "bundler/setup"
-
-require 'git'
-require 'logger'
+require File.expand_path('../lib', __FILE__)
 
 module Git
 
   class Process
-    attr_reader :logger, :git
+    attr_reader :lib
 
     def initialize(dir, logger = nil)
-      if logger == nil
-        Logger.new(STDOUT)
-        logger.level = Logger::INFO
-      end
-      @logger = logger
-      workdir = File.expand_path(dir)
-      if File.directory?(File.join(dir, '.git'))
-        @git = Git.open(workdir, :log => logger)
-      else
-        @git = Git.init(workdir, :log => logger)
-      end
-    end
-
-
-    def workdir
-      git.dir.to_s
-    end
-
-
-    def add(file)
-      git.add(file)
-    end
-
-
-    def commit(msg)
-      git.commit(msg)
-    end
-
-
-    def clone(source_repo, clone_dir)
-      cr = Git.clone(source_repo, 'temprepo', :path => clone_dir, :log => logger)
-      Git::Process.new(cr.dir.to_s, logger)
+      @lib = PLib.new(dir, logger)
     end
 
 
     def rebase_to_master(remote = true)
-      command('fetch', '-p') if remote
+      lib.fetch if remote
       rebase(if remote then "origin/master" else "master" end)
     end
 
 
     def rebase(base)
       begin
-        command('rebase', base)
+        lib.rebase(base)
       rescue => rebase_error_message
         handle_rebase_error(rebase_error_message)
       end
     end
 
 
-    def rebase_continue
-      command('rebase', '--continue')
+    def git
+      lib.git
     end
 
 
     private
 
-    def command(cmd, opts = [], chdir = true, redirect = '')
-      git.lib.send(:command, cmd, opts, chdir, redirect)
+
+    def logger
+      @lib.logger
     end
 
 
@@ -77,13 +43,14 @@ module Git
 
       git.status.each do |status|
         if remerged_file?(status, rebase_error_message)
-          add(status.path)
+          lib.add(status.path)
         end
       end
 
-      if git.status.all? {|status| status.stage == '0'}
-        rebase_continue
+      if lib.clean_status?
+        lib.rebase_continue
       end
+
     end
 
 
---
+++ lib/lib.rb
@@ -0,0 +1,70 @@
+require "rubygems"
+require "bundler/setup"
+
+require 'git'
+require 'logger'
+
+module Git
+
+  class PLib
+    attr_reader :logger, :git
+
+    def initialize(dir, logger = nil)
+      if logger == nil
+        logger = Logger.new(STDOUT)
+        logger.level = Logger::INFO
+      end
+      @logger = logger
+      workdir = File.expand_path(dir)
+      @git = if File.directory?(File.join(workdir, '.git'))
+        Git.open(workdir, :log => logger)
+      else
+        Git.init(workdir, :log => logger)
+      end
+    end
+
+
+    def workdir
+      git.dir.to_s
+    end
+
+
+    def add(file)
+      git.add(file)
+    end
+
+
+    def commit(msg)
+      git.commit(msg)
+    end
+
+
+    def rebase(base)
+      command('rebase', base)
+    end
+
+
+    def fetch
+      command('fetch', '-p')
+    end
+
+
+    def rebase_continue
+      command('rebase', '--continue')
+    end
+
+
+    def clean_status?
+      git.status.all? {|status| status.stage == '0'}
+    end
+
+
+    private
+
+    def command(cmd, opts = [], chdir = true, redirect = '')
+      git.lib.send(:command, cmd, opts, chdir, redirect)
+    end
+
+  end
+
+end
