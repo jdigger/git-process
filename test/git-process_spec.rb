0d5449dae1034f068080e31604507a6804b1ffd6
Jim Moore <moore.jim@gmail.com>
2012-06-07 13:20:53 -0600

    Clearer testing for git-process.

--- test/git-process_spec.rb
+++ test/git-process_spec.rb
@@ -5,7 +5,14 @@ describe Git::Process do
 
   before(:each) do
     @tmpdir = Dir.mktmpdir
+    @gp = Git::Process.new(@tmpdir, :log_level => Logger::ERROR)
+
+    gitignore = file('.gitignore')
+    FileUtils.touch gitignore
+    @gp.lib.add(gitignore)
+    @gp.lib.commit('initial')
   end
+
 
   after(:each) do
     rm_rf(@tmpdir)
@@ -17,19 +24,37 @@ describe Git::Process do
   end
 
 
+  def file(filename)
+    File.join(@gp.lib.workdir, filename)
+  end
+
+
+  def change_file_and_commit(filename, contents)
+    fa = file(filename)
+    File.open(fa, 'w') {|f| f.puts contents}
+    @gp.lib.add(fa)
+    @gp.lib.commit("#{filename} - #{contents}")
+  end
+
+
   describe "rebase to master" do
 
     it "should work easily for a simple rebase" do
-      tgz_file = File.expand_path('../files/simple-rebase.tgz', __FILE__)
-      Dir.chdir(@tmpdir) { `tar xfz #{tgz_file}` }
-      gp = Git::Process.new(@tmpdir, :log_level => Logger::ERROR)
+      @gp.lib.checkout('master', :new_branch => 'fb')
+      change_file_and_commit('a', '')
 
-      commit_count(gp).should == 2
+      commit_count(@gp).should == 2
 
-      gp.rebase_to_master(false)
+      @gp.lib.checkout('master')
+      change_file_and_commit('b', '')
 
-      commit_count(gp).should == 3
+      @gp.lib.checkout('fb')
+
+      @gp.rebase_to_master(false)
+
+      commit_count(@gp).should == 3
     end
+
 
     it "should work for a rebase after a rerere merge" do
       tgz_file = File.expand_path('../files/merge-conflict-rerere.tgz', __FILE__)
