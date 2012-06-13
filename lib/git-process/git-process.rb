d4975847089d679d6ea93e4d8b664cd2b0391cd1
Jim Moore <moore.jim@gmail.com>
2012-06-13 13:33:22 -0600

    Kinda-sorta have GitHub authentication working.

--- lib/git-process/git-process.rb
+++ lib/git-process/git-process.rb
@@ -2,6 +2,7 @@ require 'backports'
 require_relative 'git-lib'
 require_relative 'uncommitted-changes-error'
 require_relative 'git-rebase-error'
+require_relative 'pull-request'
 require 'shellwords'
 require 'oauth2'
 require 'launchy'
@@ -116,35 +117,7 @@ module Git
 
 
     def pull_request
-      client = OAuth2::Client.new('015168ac52d54750e525', '9317e6ae5a9e19f4e8140e6ef27370f102a7c324',
-                                  # :ssl => {:ca_file => '/etc/ssl/ca-bundle.pem'},
-                                  :site => 'https://api.github.com',
-                                  :authorize_url => 'https://github.com/login/oauth/authorize',
-                                  :token_url => 'https://github.com/login/oauth/access_token')
-      url = client.auth_code.authorize_url(:scope => 'repo', :redirect_uri => 'http://localhost:2000/github-oauth2-callback')
-      # Thread.new do
-        logger.info("Launching #{url} to get authorization.")
-        Launchy.open(url)
-      # end
-
-      s = HTTPServer.new(:Port => 2000)
-      s.mount_proc("/github-oauth2-callback") do |req, res|
-        res['Content-Type'] = "text/text"
-        res.body = %{
-          <html><body>
-          <p>Hello. You're calling from a #{req['User-Agent']}</p> <p>I see parameters: #{req.query.keys.join(', ')}</p>
-             </body></html>
-           }
-        Thread.new do
-          sleep(1)
-          s.shutdown
-        end
-      end
-      trap("INT"){ s.shutdown }
-      s.start
-
-      # token = client.auth_code.get_token('36089d4a700e0706177c')
-      # puts "token: #{token}"
+      Git::PullRequest.new(lib).pull_request
     end
 
 
